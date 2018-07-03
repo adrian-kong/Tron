619d5638ffead11929209e29bb014b96efc3dfde
adrian-kong <adrian.k1234567890@gmail.com>
2018-07-03 22:40:06 +1000

    Update

---
+++ src/me/game/utils/Facing.java
@@ -0,0 +1,17 @@
+package me.game.utils;
+
+/**
+ * @author Adrian
+ * @since 03/07/18
+ */
+public enum Facing {
+
+    STATIC(0, 0), UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0);
+
+    public final int dX, dY;
+
+    Facing(int dX, int dY) {
+        this.dX = dX;
+        this.dY = dY;
+    }
+}
